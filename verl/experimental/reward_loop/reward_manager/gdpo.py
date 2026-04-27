ca7432efe0b088bcc7782e45281ee1a22da8caa0
lvhaoyu <lvhaoyu@huawei.com>
2026-04-27 14:48:48 +0800

    add length penalty for gdpo

--- verl/experimental/reward_loop/reward_manager/gdpo.py
+++ verl/experimental/reward_loop/reward_manager/gdpo.py
@@ -29,8 +29,24 @@ class GDPORewardManager(RewardManagerBase):
         self.compute_score = compute_score or default_compute_score
         self.is_async_reward_score = inspect.iscoroutinefunction(self.compute_score)
 
+        overlong_buffer_cfg = config.reward.get("reward_kwargs", {}).get("overlong_buffer_cfg", None)
+        self.overlong_buffer_cfg = overlong_buffer_cfg
+        self.max_resp_len = config.reward.get("reward_kwargs", {}).get("max_resp_len", None)
         self.reward_router_address = reward_router_address
         self.reward_model_tokenizer = reward_model_tokenizer
+
+        if self.overlong_buffer_cfg is not None:
+            assert self.max_resp_len is not None, (
+                f"max_resp_len must be provided if {overlong_buffer_cfg=}, but got None"
+            )
+            assert self.max_resp_len >= self.overlong_buffer_cfg.len, (
+                "max_resp_len must be larger than overlong_buffer.len"
+            )
+            assert not self.overlong_buffer_cfg.enable or self.overlong_buffer_cfg.len > 0, (
+                "overlong_buffer.len must be positive when overlong penalty is enabled,"
+                f"but got {self.overlong_buffer_cfg.len}."
+                "To disable the overlong penalty, set overlong_buffer.enable = False"
+            )
 
     async def run_single(self, data: DataProto) -> dict:
         assert len(data) == 1, "Only support single data item"
@@ -88,5 +104,16 @@ class GDPORewardManager(RewardManagerBase):
             reward_extra_info["acc"] = score
 
         reward = score
+
+        if self.overlong_buffer_cfg is not None and self.overlong_buffer_cfg.enable:
+            overlong_buffer_len = self.overlong_buffer_cfg.len
+            expected_len = self.max_resp_len - overlong_buffer_len
+            exceed_len = valid_response_length - expected_len
+            overlong_penalty_factor = self.overlong_buffer_cfg.penalty_factor
+            overlong_reward = min(-exceed_len / overlong_buffer_len * overlong_penalty_factor, 0)
+            reward_extra_info["overlong_reward"] = overlong_reward
+            reward_extra_info["overlong_penalty_factor"] = overlong_penalty_factor
+            if self.overlong_buffer_cfg.log:
+                reward_extra_info["overlong"] = overlong_reward < 0
 
         return {"reward_score": reward, "reward_extra_info": reward_extra_info}
